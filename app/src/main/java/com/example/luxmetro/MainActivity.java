e1cd67e5fe7c7c9ff08bd22f99e0224c717c22f6
Alex Francisco <alex2007f@gmail.com>
2021-03-31 12:57:59 -0300

    Resolução do exercício app luxímetro do curso técnico em desenvolvimento de sistemas

---
+++ app/src/main/java/com/example/luxmetro/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.luxmetro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener sensorEventListener;
+    static float max;
+    static float min = 400000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if (sensor == null) {
+            Toast.makeText(this, "O dispositivo não possui sensor de luz!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+
+                if (value > max) {
+                    max = value;
+
+                }
+
+               if (value < min) {
+                   min = value;
+               }
+
+                TextView txtAtualProg = (TextView) findViewById(R.id.txtAtual);
+                TextView txtMaxProg = (TextView) findViewById(R.id.txtMax);
+                TextView txtMinProg = (TextView) findViewById(R.id.txtMin);
+                txtMaxProg.setText("Máx lx: " + max);
+                txtMinProg.setText("Min lx: " + min);
+                txtAtualProg.setText(value + " lx");
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+}
